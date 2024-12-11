5995ac1b024c90fe6bef174edff452e435c514d0
zhaparzhiger <keymav42@gmail.com>
2024-12-11 13:28:22 +0500

    dadaw

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export default function middleware(request: NextRequest) {
   const isLoggedIn = request.cookies.get('isLoggedIn')
 
   if (!isLoggedIn && request.nextUrl.pathname !== '/login') {
