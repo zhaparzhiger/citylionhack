986db618425792afcb4b52ab470f3b1bae6cb9c1
zhaparzhiger <keymav42@gmail.com>
2024-12-11 13:35:57 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        middlewarePrefetch: true, // Попробуйте отключить это, если ошибка сохраняется
+      },
+};
 
 export default nextConfig;
