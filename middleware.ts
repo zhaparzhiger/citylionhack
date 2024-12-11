249948c4730da7265eb356a113e3e2f82f40046c
zhaparzhiger <keymav42@gmail.com>
2024-12-11 14:04:46 +0500

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,5 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
-  const isLoggedIn = request.cookies.get('isLoggedIn')
-
-  if (!isLoggedIn && request.nextUrl.pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-
-  return NextResponse.next()
+export function middleware() {
+  return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-}
-
